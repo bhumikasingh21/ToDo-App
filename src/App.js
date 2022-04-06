594c44b2dbf969c7ab74e1fe348ba556cb09ad87
bhumikasingh21 <bhumikas186@gmail.com>
2022-04-06 23:23:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+const [input,setInput]=React.useState("");
+const [output,setOutput]= React.useState([]);
+
+const handleChange=(event)=>{
+  setInput(event.target.value)
+}
+
+const handleData=()=>{
+  setOutput((data)=>{
+    return [...data ,input]
+  })
+  setInput("");
+}
+
+const Delete=(id)=>{
+  console.log("delete",id)
+  const newData = output.filter((value,key)=>{
+  return key+1 !== id
+
+  })
+  console.log(newData)
+  setOutput(newData)
+}
+ console.log(output)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <> 
+  <div className='todo'>
+    <h2>TO DO LIST</h2>
+    <div className='todoinput'>
+    <input type="text" placeholder='Add item' className='todo-input' value={input} onChange={handleChange}/>
+    <button className='btn' onClick={handleData}>+</button>
     </div>
+    <div >
+
+      {
+        output.map((value,key)=>{
+          return <div className='todo-output'>
+           <h2>{value} </h2>
+          <button className='btn' onClick={()=>Delete(key+1)}>x</button>
+          
+      </div>       }) 
+      }
+      <br/>
+    </div>
+  </div>
+  </>
   );
 }
 
